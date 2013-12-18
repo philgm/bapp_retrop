8263b2323e5f86b6a3a05b7aad00f576fb02b65e
philgm <philippe.marchal@ifsttar.fr>
2013-12-18 16:54:39 +0100

    timeout 1s, pour voir

--- www/js/index.js
+++ www/js/index.js
@@ -28,7 +28,7 @@ $(function(){
 			type: 'GET',
 			url: 'http://www.argosapps.fr/bapp_retrop/server/bugs.php?&jsoncallback=?',
 			dataType: 'JSONp',
-			timeout: 5000,
+			timeout: 1000,
 			success: function(data) {
 				$.each(data, function(i,item){
 					bugs.append('<li>'+item.pseudo);
